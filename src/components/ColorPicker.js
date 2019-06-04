6030c9f946e996350078f933db093dd503666af8
KillahBea <bee.noelle6@gmail.com>
2019-06-04 14:13:53 -0400

    added alpha and button to randomize

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -2,19 +2,20 @@ import React, { useState } from 'react'
 // import { clearScreenDown } from 'readline'
 
 export default function ColorPicker() {
-  const [hue, setHue] = useState(180)
-  const [sat, setSat] = useState(50)
-  const [light, setLight] = useState(50)
+  const [hue, setHue] = useState(Math.floor(Math.random() * 360))
+  const [sat, setSat] = useState(Math.floor(Math.random() * 100))
+  const [light, setLight] = useState(Math.floor(Math.random() * 100))
+  const [alpha, setAlpha] = useState(Math.floor(Math.random() * 100))
   console.log(hue)
   console.log(sat)
   console.log(light)
-  // console.log('dang girl')
-  // const updateColorSwatch = event
 
-  // const getHSL = () => {
-  //   return `hsl(${hue}, ${sat}%, ${light}%)`
-  //   console.log('getHSL')
-  // }
+  const getRandom = () => {
+    setHue(Math.floor(Math.random() * 320))
+    setSaturation(Math.floor(Math.random() * 100))
+    setLightness(Math.floor(Math.random() * 100))
+    setAlpha(Math.floor(Math.random() * 100))
+  }
   return (
     <div>
       <div className="swatch" style={{ backgroundColor: `hsl(${hue}, ${sat}%, ${light}%)` }} />
@@ -43,6 +44,19 @@ export default function ColorPicker() {
           max="100"
           onChange={event => setLight(event.target.value)}
         />
+        <div>
+          <h6>ALPHA</h6>
+          <input
+            name="alpha"
+            type="range"
+            min="1"
+            max="100"
+            onChange={event => setAlpha(event.target.value)}
+          />
+        </div>
+        <button onClick={getRandom}>
+          <span role="img">🌈</span>
+        </button>
       </div>
     </div>
   )
